b94ae15a2f6065492555e3d04d9feadfaf21a92c
Valiec <valiec2019@gmail.com>
2022-02-22 23:29:56 -0500

    Setting up repo.

---
+++ src/main/java/net/ludocrypt/example/ExampleMod.java
@@ -0,0 +1,12 @@
+package net.ludocrypt.example;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleMod implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+
+	}
+
+}
