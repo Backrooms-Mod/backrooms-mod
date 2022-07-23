a424b26a092494d719fbee15952c6f70d8465506
Valiec <valiec2019@gmail.com>
2022-07-23 18:05:04 -0400

    Switched off the shader so I can see properly

--- src/main/java/com/kpabr/backrooms/init/BackroomsLevels.java
+++ src/main/java/com/kpabr/backrooms/init/BackroomsLevels.java
@@ -41,9 +41,9 @@ public class BackroomsLevels {
 
     public static final RegistryKey<Biome> COMMUNAL_CORRIDORS_BIOME = get("communal_corridors", CommunalCorridorsBiome.create());
 
+    //of(new StrongLiminalShader(BackroomsMod.id("communal_corridors")))
 
-
-    public static final LiminalEffects COMMUNAL_CORRIDORS_EFFECTS = new LiminalEffects(Optional.of(new LiminalBaseEffects.SimpleBaseEffects(Optional.empty(), false, "NONE", true, false, false)), Optional.of(new StrongLiminalShader(BackroomsMod.id("communal_corridors"))), Optional.of(new LiminalSkyRenderer.SkyboxSky(BackroomsMod.id("textures/sky/snow"))), Optional.of(new MusicSound(BackroomsSoundEvents.MUSIC_COMMUNAL_CORRIDORS, 3000, 8000, true)), Optional.of(new ReverbSettings().setDecayTime(2.15F).setDensity(0.725F)));
+    public static final LiminalEffects COMMUNAL_CORRIDORS_EFFECTS = new LiminalEffects(Optional.of(new LiminalBaseEffects.SimpleBaseEffects(Optional.empty(), false, "NONE", true, false, false)), Optional.empty(), Optional.of(new LiminalSkyRenderer.SkyboxSky(BackroomsMod.id("textures/sky/snow"))), Optional.of(new MusicSound(BackroomsSoundEvents.MUSIC_COMMUNAL_CORRIDORS, 3000, 8000, true)), Optional.of(new ReverbSettings().setDecayTime(2.15F).setDensity(0.725F)));
     public static final LiminalWorld COMMUNAL_CORRIDORS = get("communal_corridors", new LiminalWorld(BackroomsMod.id("communal_corridors"), DimensionType.create(OptionalLong.of(23500), true, false, false, true, 1.0, false, false, true, false, false, 0, 128, 128, TagKey.of(Registry.BLOCK_KEY, BackroomsMod.id("communal_corridors")), BackroomsMod.id("communal_corridors"), 0.075F), (world, dimensionTypeRegistry, biomeRegistry, structureRegistry, chunkGeneratorSettingsRegistry, noiseSettingsRegistry, registryManager, seed) -> new DimensionOptions(dimensionTypeRegistry.getOrCreateEntry(world.getDimensionTypeKey()), new CommunalCorridorsChunkGenerator(new FixedBiomeSource(biomeRegistry.getOrCreateEntry(BackroomsLevels.COMMUNAL_CORRIDORS_BIOME)), seed)), COMMUNAL_CORRIDORS_EFFECTS));
 
     public static void init() {
