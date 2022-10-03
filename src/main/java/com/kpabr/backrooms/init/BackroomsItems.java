c5ee5d3964520b07659a974de3cdafb283343c97
JsMeansJauhesammutin <67021088+JsMeansJauhesammutin@users.noreply.github.com>
2022-10-03 18:54:03 +0300

    Hound Stuff
    known bugs:
    hound spawn egg no work fsr (commented)
    tainted flesh no give hunger effect
    hounds op (not bug)

--- src/main/java/com/kpabr/backrooms/init/BackroomsItems.java
+++ src/main/java/com/kpabr/backrooms/init/BackroomsItems.java
@@ -4,8 +4,11 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import com.kpabr.backrooms.items.FireSalt;
+//import com.kpabr.backrooms.init.BackroomsEntities.HOUND;
 import com.kpabr.backrooms.BackroomsMod;
 import com.kpabr.backrooms.items.SpecialWaterItem;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -20,7 +23,10 @@ public class BackroomsItems {
 	public static final Item ALMOND_WATER = add("almond_water", new SpecialWaterItem(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().alwaysEdible().snack().saturationModifier(20).hunger(4).build()).maxCount(16)));
 	public static final Item FIRESALT = add("firesalt", new FireSalt(new Item.Settings().group(ItemGroup.MISC)));
 	public static final Item TILEMOLD_LUMP = add("tilemold_lump", new Item(new Item.Settings().group(ItemGroup.MISC)));
-	public static final Item BAKED_TILEMOLD_LUMP = add("baked_tilemold_lump", new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().snack().saturationModifier(2).hunger(2).build()).maxCount(64)));
+	public static final Item BAKED_TILEMOLD_LUMP = add("baked_tilemold_lump", new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().snack().saturationModifier(2).hunger(2).statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 600, 0), 0.8F).meat().build())));
+	public static final Item TAINTED_FLESH = add("tainted_flesh", new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(0).hunger(3).build())));
+	public static final Item COOKED_FLESH = add("cooked_flesh", new Item(new Item.Settings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().saturationModifier(3).hunger(5).build())));
+	//public static final Item HOUND_SPAWN_EGG = add("hound_spawn_egg", new SpawnEggItem(HOUND,16777215,8531483,new Item.Settings().group(ItemGroup.MISC)));
 
 
 	private static <I extends Item> I add(String name, I item) {
