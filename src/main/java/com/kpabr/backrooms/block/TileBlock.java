74fe5396d284b53364c4ada4b8b272351dd7d3a4
MegDuck <65822271+MegDuck@users.noreply.github.com>
2022-10-18 20:15:33 +0800

    Removed old cork tile behaviour

--- src/main/java/com/kpabr/backrooms/block/TileBlock.java
+++ src/main/java/com/kpabr/backrooms/block/TileBlock.java
@@ -12,45 +12,7 @@ import net.minecraft.world.World;
 
 public class TileBlock extends Block {
 
-	public static final DirectionProperty FACING = Properties.FACING;
-
 	public TileBlock(Settings settings) {
 		super(settings);
-		setDefaultState(getStateManager().getDefaultState().with(FACING, Direction.UP));
 	}
-
-	@Override
-	protected void appendProperties(StateManager.Builder<Block, BlockState> stateManager) {
-		stateManager.add(FACING);
-	}
-
-	@Override
-	public BlockState getPlacementState(ItemPlacementContext ctx) {
-		World world = ctx.getWorld();
-		BlockPos pos = ctx.getBlockPos();
-		BlockState hitState = world.getBlockState(pos.mutableCopy().move(ctx.getSide().getOpposite()));
-		Direction hit = ctx.getSide();
-		Direction look = ctx.getPlayerLookDirection();
-		//the code after this comment is rendered pointless by a new change. however, certain things may depend on it. clean up before final release.
-		if (hitState.isOf(this)) {
-			Direction stateFace = hitState.get(FACING);
-
-			if (!stateFace.getAxis().equals(hit.getAxis())) {
-				if (ctx.getPlayer().isSneaking()) {
-					return this.getDefaultState().with(FACING, stateFace.getOpposite());
-				} else {
-					return this.getDefaultState().with(FACING, look.getOpposite());
-				}
-			} else {
-				if (ctx.getPlayer().isSneaking()) {
-					return this.getDefaultState().with(FACING, look.getOpposite());
-				} else {
-					return this.getDefaultState().with(FACING, stateFace.getOpposite());
-				}
-			}
-		} else {
-			return this.getDefaultState().with(FACING, ctx.getPlayerLookDirection().getOpposite());
-		}
-	}
-
 }
