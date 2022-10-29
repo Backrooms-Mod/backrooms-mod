5f605c4eb8af26589b23c5d428993978aef0611b
MegDuck <65822271+MegDuck@users.noreply.github.com>
2022-10-30 00:31:24 +0800

    Removed sounds from the corners mod

--- src/main/java/com/kpabr/backrooms/init/BackroomsSoundEvents.java
+++ src/main/java/com/kpabr/backrooms/init/BackroomsSoundEvents.java
@@ -2,20 +2,9 @@ package com.kpabr.backrooms.init;
 import net.minecraft.sound.SoundEvent;
 import static com.kpabr.backrooms.util.RegistryHelper.get;
 public class BackroomsSoundEvents {
-
-    //public static final SoundEvent MUSIC_COMMUNAL_CORRIDORS = get("music.communal_corridors");
-
     public static SoundEvent HUMBUZZ_LEVEL_0;
-
     public static void init() {
         HUMBUZZ_LEVEL_0 = get("humbuzz");
     }
-
-    // Radio
-    //public static final SoundEvent RADIO_COMMUNAL_CORRIDORS = get("radio.communal_corridors");
-
-
-    // Ambient
-    //public static final SoundEvent BIOME_LOOP_COMMUNAL_CORRIDORS = get("biome.communal_corridors.loop");
 
 }
