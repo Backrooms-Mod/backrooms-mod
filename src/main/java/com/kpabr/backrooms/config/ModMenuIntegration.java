a9bb88b1fbcf48198731e34349af0a2e721ed656
MegDuck <65822271+MegDuck@users.noreply.github.com>
2022-10-29 12:21:40 +0800

    ModMenu integration

---
+++ src/main/java/com/kpabr/backrooms/config/ModMenuIntegration.java
@@ -0,0 +1,12 @@
+package com.kpabr.backrooms.config;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(BackroomsConfig.class, parent).get();
+    }
+}
