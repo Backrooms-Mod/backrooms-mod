7071a052c81d59b30084f5f141802fb272de09f2
MegDuck <65822271+MegDuck@users.noreply.github.com>
2022-10-30 22:13:22 +0800

    Fixed generation algorithm

--- src/main/java/com/kpabr/backrooms/world/biome/LevelZeroBiomeSource.java
+++ src/main/java/com/kpabr/backrooms/world/biome/LevelZeroBiomeSource.java
@@ -1,6 +1,7 @@
 package com.kpabr.backrooms.world.biome;
 
 import com.google.common.collect.ImmutableList;
+import com.kpabr.backrooms.BackroomsMod;
 import com.kpabr.backrooms.init.BackroomsLevels;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
@@ -42,7 +43,6 @@ public class LevelZeroBiomeSource extends BiomeSource {
         this.normalBiome = normalBiome;
         this.crimsonWallsBiome = crimsonWallsBiome;
         ChunkRandom chunkRandom = new ChunkRandom(new AtomicSimpleRandom(seed));
-        chunkRandom.skip(17292);
         this.noise = new SimplexNoiseSampler(chunkRandom);
     }
 
@@ -61,7 +61,8 @@ public class LevelZeroBiomeSource extends BiomeSource {
     public RegistryEntry<Biome> getBiome(int x, int y, int z, MultiNoiseUtil.MultiNoiseSampler noise) {
         int i = x >> 2;
         int j = z >> 2;
-        float noiseAt = LevelZeroBiomeSource.getNoiseAt(this.noise, i * 2 + 1, j * 2 + 1);
+        float noiseAt = LevelZeroBiomeSource.getNoiseAt(this.noise, i, j);
+        BackroomsMod.LOGGER.info(String.valueOf(noiseAt));
         if (noiseAt > 40.0f) {
             return this.crimsonWallsBiome;
         }
@@ -75,23 +76,9 @@ public class LevelZeroBiomeSource extends BiomeSource {
     public static float getNoiseAt(SimplexNoiseSampler simplexNoiseSampler, int i, int j) {
         int k = i / 2;
         int l = j / 2;
-        int m = i % 2;
-        int n = j % 2;
-        float f = 100.0f - MathHelper.sqrt(i * i + j * j) * 8.0f;
+
+        float f = MathHelper.sqrt(i * i + j * j) * 8.0f;
         f = MathHelper.clamp(f, -100.0f, 80.0f);
-        for (int o = -12; o <= 12; ++o) {
-            for (int p = -12; p <= 12; ++p) {
-                long q = k + o;
-                long r = l + p;
-                if (q * q + r * r <= 4096L || !(simplexNoiseSampler.sample(q, r) < (double)-0.9f)) continue;
-                float g = (MathHelper.abs(q) * 3439.0f + MathHelper.abs(r) * 147.0f) % 13.0f + 9.0f;
-                float h = m - o * 2;
-                float s = n - p * 2;
-                float t = 100.0f - MathHelper.sqrt(h * h + s * s) * g;
-                t = MathHelper.clamp(t, -100.0f, 80.0f);
-                f = Math.max(f, t);
-            }
-        }
         return f;
     }
 }
