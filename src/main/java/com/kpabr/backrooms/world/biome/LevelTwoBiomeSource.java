4a461e9361c08f455649b5efe86dc74524216d0a
Trimsku <69107937+Trimsku@users.noreply.github.com>
2022-12-24 14:20:53 +0300

    Just nothing

--- src/main/java/com/kpabr/backrooms/world/biome/LevelTwoBiomeSource.java
+++ src/main/java/com/kpabr/backrooms/world/biome/LevelTwoBiomeSource.java
@@ -6,16 +6,11 @@ import com.kpabr.backrooms.util.BiomeRegistryList;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.dynamic.RegistryOps;
-import net.minecraft.util.math.noise.SimplexNoiseSampler;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.source.BiomeSource;
-import net.minecraft.world.biome.source.util.MultiNoiseUtil;
-import net.minecraft.world.gen.random.AtomicSimpleRandom;
-import net.minecraft.world.gen.random.ChunkRandom;
 
-public class LevelTwoBiomeSource extends BiomeSource {
+public class LevelTwoBiomeSource extends BaseBiomeSource {
     public static final Codec<LevelTwoBiomeSource> CODEC = RecordCodecBuilder.create((instance) ->
             instance.group(
                     RegistryOps.createRegistryCodec(Registry.BIOME_KEY)
@@ -25,14 +20,10 @@ public class LevelTwoBiomeSource extends BiomeSource {
                             .forGetter((biomeSource) -> biomeSource.seed)
             ).apply(instance, instance.stable(LevelTwoBiomeSource::new)));
 
-    private final SimplexNoiseSampler noise;
-    protected final long seed;
-
-    protected final BiomeRegistryList biomeList;
     protected Registry<Biome> BIOME_REGISTRY;
 
     public LevelTwoBiomeSource(Registry<Biome> biomes, Long seed) {
-        this(biomes, seed, new BiomeListBuilder()
+        super(biomes, seed, new BiomeListBuilder()
                 .addBiome(BackroomsLevels.PIPES_BIOME, BiomeRegistryList.DEFAULT_CHANCE_VALUE)
         );
     }
@@ -41,29 +32,8 @@ public class LevelTwoBiomeSource extends BiomeSource {
     public BiomeSource withSeed(long seed) {
         return new Level0BiomeSource(BIOME_REGISTRY, seed);
     }
-
-
-    public LevelTwoBiomeSource(Registry<Biome> biomeRegistry, long seed, BiomeListBuilder biomeList) {
-        this(seed, BiomeRegistryList.from(biomeRegistry, biomeList));
-        this.BIOME_REGISTRY = biomeRegistry;
-    }
-
-    protected LevelTwoBiomeSource(long seed, BiomeRegistryList biomeList) {
-        super(biomeList.getBiomeEntries());
-
-        this.seed = seed;
-        this.biomeList = biomeList;
-
-        final ChunkRandom chunkRandom = new ChunkRandom(new AtomicSimpleRandom(seed));
-        this.noise = new SimplexNoiseSampler(chunkRandom);
-    }
     @Override
     protected Codec<? extends BiomeSource> getCodec() {
         return CODEC;
-    }
-
-    @Override
-    public RegistryEntry<Biome> getBiome(int x, int y, int z, MultiNoiseUtil.MultiNoiseSampler noise) {
-        return null;
     }
 }
