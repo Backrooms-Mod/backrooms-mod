93c62b00544972bd956ce0ec876eb98bb631c3a3
Trimsku <69107937+Trimsku@users.noreply.github.com>
2023-02-20 07:23:34 +0300

    Crate block refactored

--- src/main/java/com/kpabr/backrooms/block/CrateBlock.java
+++ src/main/java/com/kpabr/backrooms/block/CrateBlock.java
@@ -2,31 +2,24 @@ package com.kpabr.backrooms.block;
 
 import java.util.Random;
 import net.minecraft.block.*;
-import net.minecraft.block.AbstractBlock.Settings;
 import com.kpabr.backrooms.block.entity.CrateBlockEntity;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.mob.PiglinBrain;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
-import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.stat.Stats;
 import net.minecraft.state.StateManager.Builder;
 import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
-import net.minecraft.state.property.Property;
 import net.minecraft.util.ActionResult;
-import net.minecraft.util.BlockMirror;
-import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
@@ -35,9 +28,10 @@ public class CrateBlock extends BlockWithEntity {
 
     public CrateBlock(Settings settings) {
         super(settings);
-        this.setDefaultState((BlockState)this.stateManager.getDefaultState().with(OPEN, false));
+        this.setDefaultState(this.stateManager.getDefaultState().with(OPEN, false));
     }
 
+    @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (world.isClient) {
             return ActionResult.SUCCESS;
@@ -53,6 +47,7 @@ public class CrateBlock extends BlockWithEntity {
         }
     }
 
+    @Override
     public void onStateReplaced(BlockState state, World world, BlockPos pos, BlockState newState, boolean moved) {
         if (!state.isOf(newState.getBlock())) {
             BlockEntity blockEntity = world.getBlockEntity(pos);
@@ -60,19 +55,19 @@ public class CrateBlock extends BlockWithEntity {
                 ItemScatterer.spawn(world, pos, (Inventory)blockEntity);
                 world.updateComparators(pos, this);
             }
-
             super.onStateReplaced(state, world, pos, newState, moved);
         }
     }
 
+    @Override
     public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
         BlockEntity blockEntity = world.getBlockEntity(pos);
-        if (blockEntity instanceof CrateBlockEntity) {
-            ((CrateBlockEntity)blockEntity).tick();
+        if (blockEntity instanceof CrateBlockEntity crateBlockEntity) {
+            crateBlockEntity.tick();
         }
-
     }
 
+    @Override
     @Nullable
     public BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
         return new CrateBlockEntity(pos, state);
@@ -85,8 +80,8 @@ public class CrateBlock extends BlockWithEntity {
     public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
         if (itemStack.hasCustomName()) {
             BlockEntity blockEntity = world.getBlockEntity(pos);
-            if (blockEntity instanceof CrateBlockEntity) {
-                ((CrateBlockEntity)blockEntity).setCustomName(itemStack.getName());
+            if (blockEntity instanceof CrateBlockEntity crateBlockEntity) {
+                crateBlockEntity.setCustomName(itemStack.getName());
             }
         }
 
@@ -101,7 +96,7 @@ public class CrateBlock extends BlockWithEntity {
     }
 
     protected void appendProperties(Builder<Block, BlockState> builder) {
-        builder.add(new Property[]{OPEN});
+        builder.add(OPEN);
     }
 
     static {
